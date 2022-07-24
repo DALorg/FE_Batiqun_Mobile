8605789c0b6ef9bd393372fb7bcc6cd46033cf34
deniwahyud <deni69wahyudi@gmail.com>
2022-07-24 22:51:49 +0700

    Update EditProfileComponent.js

--- components/ProfileComponent/EditProfileComponent.js
+++ components/ProfileComponent/EditProfileComponent.js
@@ -407,7 +407,7 @@ const EditProfileComponent = ({ loading, error, profile, bitSuccessEdit }) => {
                     <div className="row">
                       <div className="col-lg-6 text-start">
                         <Link href={{ pathname: "/Profile" }}>
-                          <a className="mt-4 text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-[#9b6b43] dark:hover:bg-[#744c24] dark:focus:ring-blue-800 inline-flex ">
+                          <a className="mt-4 text-white bg-[#9b6b43] hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-[#9b6b43] dark:hover:bg-[#744c24] dark:focus:ring-blue-800 inline-flex ">
                             Cancel
                           </a>
                         </Link>
@@ -432,5 +432,5 @@ const EditProfileComponent = ({ loading, error, profile, bitSuccessEdit }) => {
     </main>
   );
 };
-    
+
 export default EditProfileComponent;
